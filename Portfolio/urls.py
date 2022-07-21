d40bb267ec9cac738c6e10d785aec5bbc6153c80
JonMarkM <mwigojm@gmail.com>
2022-07-21 14:46:38 +0300

    updated urls

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import indexView, successView, imageUploadView
+from .views import indexView, successView
 
 app_name= 'Portifolio'
 
 urlpatterns = [
     path('', indexView, name='home'),
     path('successful/', successView, name='success'),
-    path('mysiteUpload/', imageUploadView, name='upload'),
 ]
